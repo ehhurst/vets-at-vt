c976c2157cee1cd1e63d6d81cee0f9b9c8f8ab73
ehurst96 <ehurst96@vt.edu>
2026-01-12 16:33:54 -0500

    Spacing fixes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,21 +1,23 @@
 'use client';
+
 import Image from "next/image";
+
 import NavBar from "../nav/NavBar";
 import Link from "next/link";
+
 
 export default function Header() {
     return(
         <header className="w-full flex items-center justify-between px-8 shadow-md bg-white dark:bg-black">
-
             {/* Logo and site title- title is hidden for mobile, both link to homepage */}
             <Link href="/" className="flex items-center gap-4">
                 <div className="flex items-center gap-4">
                     <Image
-                    src="/veteransvtlogo.png"
-                    alt="Veterans at Virginia Tech logo"
-                    width={75}
-                    height={15}
-                    priority
+                        src="/veteransvtlogo.png"
+                        alt="Veterans at Virginia Tech logo"
+                        width={75}
+                        height={15}
+                        priority
                     />
                     <h1 className="hidden md:block text-vt-maroon font-vt-extrabold text-3xl font-bold">Veterans@VT</h1>
                 </div>
