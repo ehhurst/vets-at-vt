5f13685a42305d6559012b5911114e8b78ec77d9
ehurst96 <ehurst96@vt.edu>
2026-01-15 11:10:46 -0500

    altered officer types + added fake data, to be replaced with backend and db calls

---
+++ src/types/Officer.ts
@@ -0,0 +1,9 @@
+export type Officer = {
+    headshot_href: string,
+    name: string,
+    role: string,
+    role_type: string,
+    email: string, 
+    description: string,
+    branch?: string | undefined
+}
